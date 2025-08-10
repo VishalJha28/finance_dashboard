d5e6393c6a665c624b9a4ee66ff018ce6eeae1be
Vishal Jha <67006844+VishalJha28@users.noreply.github.com>
2025-08-10 18:49:18 +0530

    Create xirr.js

---
+++ src/utils/xirr.js
@@ -0,0 +1,3 @@
+// Placeholder: XIRR calculation implementation can be added here.
+// For now this file exports a stub that throws if used so user knows it's a TODO.
+export function calculateXIRR(){ throw new Error('XIRR not implemented yet'); }
